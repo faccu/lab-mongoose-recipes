e42128de0f4615cb568d988e3ba6c32f3214b8cf
Facundo Saracco <facundosaracco@gmail.com>
2021-11-12 10:49:22 +0100

    kinda solved

--- index.js
+++ index.js
@@ -5,23 +5,63 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb+srv://faccu:thisshitboring@cluster0.k2htr.mongodb.net/recipesLab?retryWrites=true&w=majority';
 
 // Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(self => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+
+  mongoose
+    .connect(MONGODB_URI, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    .then(self => {
+      // console.log(`Connected to the database: "${self.connection.name}"`);
+
+      // ITERATION 02
+
+      const newRecipe = {
+        title: "Tinga de pollo",
+        level: "Easy Peasy",
+        ingredients: ["Pechuga de pollo", "Tomate triturado", "Cebolla", "Chile chipotle", "Oregano" ],
+        cuisine: "Mexicano",
+        dishType: "main_course",
+        image: "https://images.media-allrecipes.com/images/75131.jpg",
+        duration: 40,
+        creator: "Facu",
+        created: new Date (2021, 11, 11)
+      }
+
+      .then(() => {
+        return Recipe.create(newRecipe);
+      })
+
+      // ITERATION 03
+
+      .then(() => {
+        return Recipe.insertMany(data);
+      })
+
+      // ITERATION 04
+
+      .then(() => {
+        return Recipe.findOneAndUpdate({title:"Rigatoni alla Genovese"}, {duration:100});
+      })
+
+      // ITERATION 05
+
+      .then(() => {
+        return Recipe.deleteOne({title: "Carrot Cake"});
+      })
+      
+    })
+
+    // ITERATION 06
+
+    .then(() => {
+      console.log("Disconnecting from the database...");
+      mongoose.disconnect();
+    })
+    .catch(error => {
+      console.error('Error connecting to the database', error);
+    });
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,18 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: { type: String, require: true, unique: true },
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"], 
+  },
+  ingredients: [{ type: String }],
+  cuisine: { type: String, require: true },
+  dishType: { type: String },
+  image: { type: String },
+  duration: { type: Number, minimum: 0 },
+  creator: { type: String },
+  created: { type: Date }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
